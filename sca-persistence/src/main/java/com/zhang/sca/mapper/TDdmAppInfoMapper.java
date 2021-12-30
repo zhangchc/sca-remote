9639b88e21820fcc6db52482b12ae96dc5197189
zhangchuancun <zhangchuancun@111.com.cn>
2021-12-30 16:37:38 +0800

    run

--- sca-persistence/src/main/java/com/zhang/sca/mapper/TDdmAppInfoMapper.java
+++ sca-persistence/src/main/java/com/zhang/sca/mapper/TDdmAppInfoMapper.java
@@ -3,8 +3,8 @@ package com.zhang.sca.mapper;
 import com.zhang.sca.model.po.TDdmAppInfo;
 import org.apache.ibatis.annotations.Param;
 
-//public interface TDdmAppInfoMapper  extends ExpandBaseMapper<TDdmAppInfo> {
-public interface TDdmAppInfoMapper  {
+public interface TDdmAppInfoMapper  extends ExpandBaseMapper<TDdmAppInfo> {
+//public interface TDdmAppInfoMapper  {
 
     int insertSelective(TDdmAppInfo record);
 
