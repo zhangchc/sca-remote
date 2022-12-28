d721803d7c5641c5ff6aca9bb228c1f3ddbf7d28
zhangchuancun <zhangchuancun@111.com.cn>
2022-12-28 14:11:22 +0800

    1

--- sca-remote-start/src/main/java/com/zhang/sca/remote/adapter/CustomerController.java
+++ sca-remote-start/src/main/java/com/zhang/sca/remote/adapter/CustomerController.java
@@ -3,6 +3,7 @@ package com.zhang.sca.remote.adapter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class CustomerController {
 
-    @RequestMapping("/getUserInfo")
+    @RequestMapping(value="/getUserInfo",method= RequestMethod.POST)
     public void getUserInfo(@RequestParam Long id) {
         log.info("======id=" + id);
     }
