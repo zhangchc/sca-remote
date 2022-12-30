552b44d3ca0c2ea7ca3f6d6300340c191c5c51f9
zhangchuancun <zhangchuancun@111.com.cn>
2022-12-30 20:56:19 +0800

    1

--- sca-remote-start/src/test/java/UserServiceTest.java
+++ sca-remote-start/src/test/java/UserServiceTest.java
@@ -2,19 +2,19 @@ import com.phoenix.sca.facade.api.UserInfoService;
 import com.phoenix.sca.facade.model.dto.UserInfoRequest;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 
 
-class UserServiceTest extends AbstractServiceTest{
+public class UserServiceTest extends AbstractServiceTest{
 @Autowired
 private UserInfoService userInfoService;
 
 
     @Test
-    void getTest() {
+    public void getTest() {
         UserInfoRequest user =new UserInfoRequest();
         user.setPageNo(1);
         user.setPageSize(10);
